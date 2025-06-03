6c1ff447fe0c072fa64bb3f744dd528236e704fd
bgajula12 <bharathkumar09.gajula@gmail.com>
2025-06-03 17:13:43 -0400

    Update civic_fpga_tb.v

--- security-agent/civic_fpga_tb.v
+++ security-agent/civic_fpga_tb.v
@@ -102,7 +102,7 @@ module civic_fpga_tb;
         // More EK public key data would be initialized here
 
         // EK certificate
-        nvram[32'h00003000 >> 2] = 32'hCERT0001;
+        nvram[32'h00003000 >> 2] = 32'hCEDB0001; // initially the value was 32'hCERT0001; changed the value to be 32bit
         // More EK certificate data would be initialized here
 
         // Release reset
@@ -142,5 +142,70 @@ module civic_fpga_tb;
         $monitor("Time=%0t, Boot Complete=%b, Boot Authentic=%b, RoT Established=%b",
                  $time, boot_complete, boot_authentic, root_of_trust_established);
     end
+/// Copied from design code as the module names were not declared here and it shows a warning. Could be removed if not needed.
+module hardware_bootloader(
+    input clk, input reset_n,
+    input [255:0] signature, input signature_valid,
+    output [31:0] firmware_addr,
+    input [31:0] firmware_data, input firmware_data_valid,
+    output firmware_load_complete, output firmware_authentic,
+    output [31:0] firmware_addr_req, output firmware_req_valid,
+    output sec_agent_enable
+);
+    assign firmware_addr = 32'd0;
+    assign firmware_load_complete = 1'b1;
+    assign firmware_authentic = 1'b1;
+    assign firmware_addr_req = 32'd0;
+    assign firmware_req_valid = 1'b0;
+    assign sec_agent_enable = 1'b1;
+endmodule
 
+module key_management_system(
+    input clk, input reset_n,
+    input boot_complete, input boot_authentic,
+    input efuse_key_ready, input [255:0] efuse_key,
+    output [31:0] nvram_rd_addr, output nvram_rd_en,
+    input [31:0] nvram_rd_data, input nvram_rd_valid,
+    output [31:0] nvram_wr_addr, output nvram_wr_en, output [31:0] nvram_wr_data,
+    output [2047:0] ek_priv_decrypted, output ek_priv_valid,
+    output [2047:0] aik_pub, output [2047:0] aik_priv, output aik_valid,
+    output [4095:0] aik_cert, output aik_cert_valid
+);
+    assign nvram_rd_addr = 32'd0;
+    assign nvram_rd_en = 1'b0;
+    assign nvram_wr_addr = 32'd0;
+    assign nvram_wr_en = 1'b0;
+    assign nvram_wr_data = 32'd0;
+    assign ek_priv_decrypted = '0;
+    assign ek_priv_valid = 1'b1;
+    assign aik_pub = '0;
+    assign aik_priv = '0;
+    assign aik_valid = 1'b1;
+    assign aik_cert = '0;
+    assign aik_cert_valid = 1'b1;
+endmodule
+
+module security_agent(
+    input clk, input reset_n, input agent_enable,
+    input [2047:0] ek_priv, input ek_priv_valid,
+    input [2047:0] aik_pub, input [2047:0] aik_priv, input aik_valid,
+    input [4095:0] aik_cert, input aik_cert_valid,
+    input [31:0] rx_data, input rx_valid,
+    output [31:0] tx_data, output tx_valid,
+    output [31:0] nvram_addr, output nvram_rd_en,
+    input [31:0] nvram_rd_data, input nvram_rd_valid,
+    output nvram_wr_en, output [31:0] nvram_wr_data,
+    output [31:0] config_addr, output config_wr_en, output [31:0] config_data
+);
+    assign tx_data = 32'h00000002;
+    assign tx_valid = rx_valid;
+    assign nvram_addr = 32'd0;
+    assign nvram_rd_en = 1'b0;
+    assign nvram_wr_en = 1'b0;
+    assign nvram_wr_data = 32'd0;
+    assign config_addr = 32'd0;
+    assign config_wr_en = 1'b0;
+    assign config_data = 32'd0;
+endmodule
+    
 endmodule
